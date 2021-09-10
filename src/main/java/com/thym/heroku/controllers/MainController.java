4ebe895ce5a3e9bab2b3959ba0572d89554e3d18
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-10 09:30:00 +0100

    code

--- src/main/java/com/thym/heroku/controllers/MainController.java
+++ src/main/java/com/thym/heroku/controllers/MainController.java
@@ -1,5 +1,39 @@
 package com.thym.heroku.controllers;
 
-public class MainController {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.thym.heroku.entities.Rappel;
+
+@Controller
+public class MainController {
+	static ArrayList<Rappel> listerappels=new ArrayList<Rappel>();
+@GetMapping("")
+public String rappels(Model m)
+{
+	Rappel r=new Rappel();
+	m.addAttribute("rappel", r);
+	return "index";
+}
+@PostMapping("save")
+public String sauvegarde(Model mo,@ModelAttribute("rappel") Rappel r)
+{
+if(!r.verif())
+{
+mo.addAttribute("erreur", "verifier vos champs");
+return "index";
+}
+else
+{
+	listerappels.add(r);
+	mo.addAttribute("liste",listerappels);
+	return "rappels";
+	
+}
+}
 }
--- src/main/java/com/thym/heroku/entities/Rappel.java
+++ src/main/java/com/thym/heroku/entities/Rappel.java
@@ -1,5 +1,53 @@
 package com.thym.heroku.entities;
 
 public class Rappel {
+private String titre,date,type;
+
+public Rappel(String titre, String date, String type) {
+	this.titre = titre;
+	this.date = date;
+	this.type = type;
+}
+
+public Rappel() {
+}
+
+public String getTitre() {
+	return titre;
+}
+
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+
+public String getDate() {
+	return date;
+}
+
+public void setDate(String date) {
+	this.date = date;
+}
+
+public String getType() {
+	return type;
+}
+
+public void setType(String type) {
+	this.type = type;
+}
+
+public boolean verif()
+{
+if(this.date.equals("")||this.titre.equals("")||this.type.equals(""))
+	return false;
+else if(this.titre.length()<4)
+	return false;
+else return true;
+
+}
+
+
+
+
 
 }
